70595c9aaf0ed4931c3acee4c7053ba2bf675411
foresightyj <imyuanjian@gmail.com>
2020-03-24 19:15:08 +0800

    initial

---
+++ Wox.CNKI.Translator/Main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Windows;
+using Wox.Plugin;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Windows.Forms;
+
+namespace Wox.CNKI.Translator
+{
+    public class TranslateResult
+    {
+        public int errorCode { get; set; }
+        public List<string> translation { get; set; }
+        public BasicTranslation basic { get; set; }
+        public List<WebTranslation> web { get; set; }
+    }
+
+    // 有道词典-基本词典
+    public class BasicTranslation
+    {
+        public string phonetic { get; set; }
+        public List<string> explains { get; set; }
+    }
+
+    public class WebTranslation
+    {
+        public string key { get; set; }
+        public List<string> value { get; set; }
+    }
+
+    public class Main : IPlugin
+    {
+        private PluginInitContext _context;
+        public void Init(PluginInitContext context)
+        {
+            _context = context;
+        }
+
+        public List<Result> Query(Query query)
+        {
+            return Translator.Translate(query.Search, msg =>
+            {
+                _context.API.ShowMsg(msg);
+            });
+        }
+
+    }
+}
---
+++ Wox.CNKI.Translator/Translator.cs
@@ -0,0 +1,78 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Wox.Plugin;
+
+namespace Wox.CNKI.Translator
+{
+    public static class Translator
+    {
+        public static List<Result> Translate(string q, Action<string> toast)
+        {
+            List<Result> results = new List<Result>();
+            const string ico = "Images\\youdao.ico";
+            if (string.IsNullOrWhiteSpace(q))
+            {
+                results.Add(new Result
+                {
+                    Title = "开始CNKI中英互译",
+                    SubTitle = "基于CNKI网页 API",
+                    IcoPath = ico
+                });
+                return results;
+            }
+            using (var httpClient = new HttpClient())
+            {
+                var body = new FormUrlEncodedContent(new Dictionary<string, string>() { { "searchword", q }, { "txt2", q }, { "saveID", q }, });
+                var res = httpClient.PostAsync("http://dict.cnki.net/dict_result.aspx", body).Result;
+                var html = res.Content.ReadAsStringAsync().Result;
+
+                var doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(html);
+                var nodes = doc.DocumentNode.QuerySelectorAll(".zztj font a:first-child");
+                foreach (var node in nodes)
+                {
+                    var translation = node.InnerText;
+                    var count = node.NextSiblingOfType(HtmlNodeType.Element).InnerText;
+
+                    results.Add(new Result
+                    {
+                        Title = translation,
+                        SubTitle = count,
+                        IcoPath = ico,
+                        Action = ctx =>
+                        {
+                            try
+                            {
+                                Clipboard.SetText(translation);
+                                toast("翻译已被存入剪贴板");
+                            }
+                            catch (Exception e)
+                            {
+                                toast("剪贴板打开失败，请稍后再试");
+                                return false;
+                            }
+                            return true;
+                        },
+                    });
+                }
+            }
+            return results;
+        }
+
+        public static HtmlNode NextSiblingOfType(this HtmlNode node, HtmlNodeType nodeType)
+        {
+            var next = node.NextSibling;
+            while (next != null && next.NodeType != nodeType)
+            {
+                next = next.NextSibling;
+            }
+            return next;
+        }
+    }
+}
